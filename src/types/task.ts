bb134ceadcdb2599fa054faa0cd38c31433d065e
BrionesAngel <14tenshikun14@gmail.com>
2026-03-15 13:56:27 -0600

    Register request & response added at tasks.ts in types

--- src/types/task.ts
+++ src/types/task.ts
@@ -17,3 +17,13 @@ export interface LoginRequest {
 export interface LoginResponse {
   token: string
 }
+
+export interface RegisterRequest {
+  username: string
+  email: string
+  password: string
+}
+
+export interface RegisterResponse {
+  token: string
+}
